05d85bd5053c671a81e60301ecb4cf280c80073a
Timofey Karasik <git.user.timchanir@mail.ru>
2024-04-23 18:31:39 +0300

    commit3

--- test/java/edu/innotech/Tests.java
+++ test/java/edu/innotech/Tests.java
@@ -1,35 +1,89 @@
 package edu.innotech;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class Tests {
     @Test
-    public void testCache() {
+    @DisplayName("Кэш работает")
+    public void test1_1() {
+        TestFraction fraction = new TestFraction(1,2);
+        Fractionable fractionPtoxy = Utils.cache(fraction);
+        fractionPtoxy.doubleValue();
+        fractionPtoxy.doubleValue();
+        Assertions.assertEquals(fraction.count,1);
+    }
+    @Test
+    @DisplayName("Кэш для нескольких процедур работает")
+    public void test1_2() {
+        TestFraction fraction = new TestFraction(1,2);
+        Fractionable fractionPtoxy = Utils.cache(fraction);
+        fractionPtoxy.doubleValue();
+        fractionPtoxy.toString();
+        Assertions.assertEquals(fraction.count,2);
+    }
+    @Test
+    @DisplayName("Возвращает корректные значения")
+    public void test1_3() {
+        TestFraction fraction = new TestFraction(1,2);
+        Fractionable fractionPtoxy = Utils.cache(fraction);
+        double d = fractionPtoxy.doubleValue();
+        Assertions.assertEquals(d,0.5);
+    }
 
-        Fraction f1 = new Fraction(1,2);
-        Fractionable f2;
+    @Test
+    @DisplayName("Возвращает корректные значения из кэша")
+    public void test1_4() {
+        TestFraction fraction = new TestFraction(1,2);
+        Fractionable fractionPtoxy = Utils.cache(fraction);
+        fractionPtoxy.doubleValue();
+        double d = fractionPtoxy.doubleValue();
+        Assertions.assertEquals(d,0.5);
+    }
 
-        f2= Utils.cache(f1);
+    @Test
+    @DisplayName("Кэш сбрасывается")
+    public void test2_1() {
+        TestFraction fraction = new TestFraction(1,2);
+        Fractionable fractionPtoxy = Utils.cache(fraction);
+        fractionPtoxy.doubleValue();
+        fractionPtoxy.toString();
+        fractionPtoxy.setNum(2);
+        Assertions.assertEquals(fraction.count,0);
+    }
 
+    @Test
+    @DisplayName("После сброса кэша значения корректные")
+    public void test2_2() {
+        TestFraction fraction = new TestFraction(1,2);
+        Fractionable fractionPtoxy = Utils.cache(fraction);
+        fractionPtoxy.doubleValue();
+        fractionPtoxy.toString();
+        fractionPtoxy.setNum(2);
+        double d = fractionPtoxy.doubleValue();
+        Assertions.assertEquals(d,1);
+    }
 
-        if (f1.doubleValue()==f2.doubleValue()&&f1.toString().equals(f2.toString())){
-            System.out.println("Успех значение закэшировалось");
-        }
-
-        f1.setDenum(4);
-
-        if (f1.doubleValue()!=f2.doubleValue()&&!f1.toString().equals(f2.toString())){
-            System.out.println("Успех значение осталось закэшированно после изменения исходного обьекта");
-        }
-
-        f2.setDenum(4);
-
-        if (f1.doubleValue()==f2.doubleValue()&&f1.toString().equals(f2.toString())){
-            System.out.println("Успех значение кэша переинициализировались");
-        }
-
-        System.out.println("Успех теста");
-
-        }
-
+    @Test
+    @DisplayName("Функции без кэша не влияют на кэш")
+    public void test3_1() {
+        TestFraction fraction = new TestFraction(1,2);
+        Fractionable fractionPtoxy = Utils.cache(fraction);
+        fractionPtoxy.doubleValue();
+        fractionPtoxy.toString();
+        fractionPtoxy.setDenum(4);
+        double d = fractionPtoxy.doubleValue();
+        Assertions.assertEquals(d,0.5);
+    }
+    @Test
+    @DisplayName("Функции без кэша не сбрасывают кэш")
+    public void test3_2() {
+        TestFraction fraction = new TestFraction(1,2);
+        Fractionable fractionPtoxy = Utils.cache(fraction);
+        fractionPtoxy.doubleValue();
+        fractionPtoxy.toString();
+        fractionPtoxy.setDenum(4);
+        Assertions.assertEquals(fraction.count,2);
+    }
 }
